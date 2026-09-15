0439ec68e74346ff1460693aaf02676ce1c82168
andrepprado <ghiringhelli.andre@outlook.com>
2026-09-15 10:32:54 -0300

    Created preload.js to optimize loads and refreshs. Add preload.js path for all html files

---
+++ js/preload.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const recursos = [
+        // LOGOS E ÍCONES GERAIS
+        "img/logoBranco.png",
+        "img/caixaFerramentas.png",
+
+        // ÍCONES DA PÁGINA INICIAL
+        "img/simuladorCredito.png",
+        "img/antecipacaoSipag(1).png",
+        "img/precificacaoSipag(1).png",
+        "img/cobranca.png",
+        "img/previdencia.png",
+
+        // CARTÕES SICOOBCARD
+        "img/NEW_CARD_GOLD_a.png",
+        "img/NEW_CARD_GOLD_plus_a.png",
+        "img/NEW_CARD_PLATINUM_a.png",
+        "img/NEW_CARD_BLACK_VERTH_a.png",
+        "img/NEW_CARD_BLACK_MERITH_a.png",
+        "img/NEW_CARD_BLACK_ZENITH_a.png",
+        "img/masterEmpresarial.png",
+        "img/visaEmpresarial.png"
+    ];
+
+    recursos.forEach(caminho => {
+        const imagem = new Image();
+        imagem.src = caminho;
+    });
+});
